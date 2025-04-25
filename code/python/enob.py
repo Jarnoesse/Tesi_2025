d12369e4d8f300825e933e259f7ea8a74ee988d1
Jarno Sandrin <jarnosandrin@gmail.com>
2025-04-26 00:02:18 +0200

    Aggiornamento metodi di lettura e nuovi file dati

--- code/python/enob.py
+++ code/python/enob.py
@@ -8,10 +8,9 @@ import os
 
 
 
-class EnobCalculator:
-    def __init__(self, filename, freq = 0, ):
+class EnobPlot:
+    def __init__(self, filename = "", freq = 0, ):
         self.filename = filename
-        self.freq = float(os.path.basename(filename).replace(".txt", ""))
         self.lsb = 1
         self.n_bits = 12
         self.v_fs = 0.6
@@ -19,11 +18,11 @@ class EnobCalculator:
         self.v_min = -0.6
         self.lsb = (self.v_max - self.v_min) / 2 ** 12
 
-        self.h_data, self.l_data = file_reader(self.filename)
+        
 
     
-    def show_data(self, x_data,y_data):
-        plt.scatter(x_data,y_data)
+    def show_data(self, x_data,y_data, label):
+        plt.scatter(x_data,y_data, label = label)
         
         
 
@@ -56,7 +55,7 @@ class EnobCalculator:
     
     def sine_fit(self, x, y, p0, bounds, verbose):
         try:
-            parameters, covariance = curve_fit(self.sine_function, x, y, p0=p0,)
+            parameters, covariance = curve_fit(self.sine_function, x, y, p0=p0, bounds = bounds)
             A, F, P, C = parameters
 
             if verbose:
@@ -74,22 +73,15 @@ class EnobCalculator:
 
 
 
-    def do_fit(self, freq=0, adc = "h", verbose = False, graph = False):
-        if adc == "l":
-            print("Fitting sine wave of ADCL")
-            y_data = self.l_data
-        elif adc == "h":
-            print("Fitting sine wave of ADCH")
-            y_data = self.h_data
-        else:
-            print("Error with adc selection")
+    def do_fit(self, y_data, freq=0, verbose = False, graph = False):
 
+        
 
         x_data          = np.arange(0, len(y_data), 1)
         amplitude_guess = ((max(y_data) - min(y_data)) / 2)
         offset_guess    = ((max(y_data) + min(y_data) )/ 2)
         phase_guess     = 3.14
-        frequence_guess = self.freq * 0.001
+        frequence_guess = self.freq 
         guess           = [amplitude_guess, frequence_guess,phase_guess, offset_guess]
         lower_bounds = [amplitude_guess * 0.95, self.freq * 0.999, -10, offset_guess - 10]
         upper_bounds = [amplitude_guess * 1.05, self.freq * 1.001, +10, offset_guess + 10]
@@ -102,30 +94,75 @@ class EnobCalculator:
             print(f"frequency guess {self.freq}")
             print("phase guess")
 
-        
         y_guess = self.sine_function(x_data, amplitude_guess ,frequence_guess , phase_guess, offset_guess)
-
-
-
-
 
         p, c = self.sine_fit(x_data, y_data, guess, param_bounds, verbose=verbose)
         A, F, P, C = p
 
         y_fit = self.sine_function(x_data, A, F, P, C)
+
         if graph:
-            self.show_data(x_data, y_fit)
-            self.show_data(x_data, y_data)
-            self.show_data(x_data, y_guess)
+            self.show_data(x_data, y_fit, label="Fit")
+            self.show_data(x_data, y_data, label = "Dati")
+            self.show_data(x_data, y_guess, label = "Guess")
+
+            plt.legend()
             plt.show()
 
         return 0, y_fit, y_data, p
         
 
-    def enob(self, adc = "h"):
-        result, y_fit, y_data, p = self.do_fit(self.freq, adc)
+    def enob(self, filepath = " ", adc = "h",freq = 0, graph = False, verbose = False):
+        self.freq = freq * 0.001
+        h_data, l_data = file_reader(filepath)
+    
+        if adc == "l":
+            if verbose: print("Ricavando dati da ADCL")
+            y_data = l_data
+        else:
+            if verbose: print("Ricavando dati da ADCH")
+            y_data = h_data
+
+        
+        result, y_fit, y_data, p = self.do_fit(y_data, self.freq, verbose = verbose, graph = graph)
         enob = self.fit_enob(y_fit, y_data)
         return enob
+    
+    def enob_plot(self, path):
+        txt_files = []
+        for root, dirs, files in os.walk(path):  # Walk through directory and subdirectories
+            for file in files:
+                if file.endswith('.txt'):
+                    full_path = os.path.join(root, file)
+                    txt_files.append(full_path)
+
+        enob_values = []
+        freqs = []
+
+        for file in txt_files:
+            print(f"File: {file}")
+            filename = os.path.basename(file)
+            freq_str = filename.replace('.txt', '')
+
+            try:
+                freq = float(freq_str)  # Convert filename to float
+            except ValueError:
+                print(f"Skipping file {file}, filename does not represent a valid frequency.")
+                continue
+
+            freqs.append(freq)
+            enob_val = self.enob(file, adc="l", freq=freq)
+            enob_values.append(enob_val)
+
+        plt.scatter(freqs, enob_values)
+        plt.xlabel("Frequency")
+        plt.ylabel("ENOB")
+        plt.title("ENOB vs Frequency")
+        plt.grid(True)
+        plt.show()
+
+        return enob_values
+        
     
         
 
--- code/python/test.py
+++ code/python/test.py
@@ -1,6 +1,6 @@
-from enob import EnobCalculator
+from enob import EnobPlot
 
-test = EnobCalculator("/home/jarno/Documents/Tesi_2025/data/0.517578125.txt")
+test = EnobPlot("/home/jarno/Documents/Tesi_2025/data/0.517578125.txt")
 
-print(test.enob())
+print(test.enob_plot("/home/jarno/Documents/Tesi_2025/scan-enob"))
 
